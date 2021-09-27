4201bf9c24acefef0e33fffaebb8eff5b96bf8b0
Julian Çuni <julian.cuni@microservices.al>
2021-09-27 07:59:14 +0200

    Some clean up

--- src/app/helpers/api.ts
+++ src/app/helpers/api.ts
@@ -5,13 +5,13 @@ export class Api {
   private static sdk: Appwrite | null;
 
   static provider() {
-    if (this.sdk) return this.sdk;
-    let client = new Appwrite();
-    client
-      .setEndpoint(Server.endpoint)
-      .setProject(Server.project)
-      .setLocale('en-US');
-    this.sdk = client;
-    return this.sdk;
+    if (!this.sdk) {
+      let client = new Appwrite();
+      client
+        .setEndpoint(environment.APPWRITE_ENDPOINT)
+        .setProject(environment.APPWRITE_PROJECTID);
+      this.sdk = client;
+    }
+    return this.sdk
   }
 }
